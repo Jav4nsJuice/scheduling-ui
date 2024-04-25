9b4353f170b0ca671ecfc8415fd24ee18d1b72e5
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-25 16:15:32 -0400

    feature/S-3: Updated the imports and the html files in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { HeaderComponent } from './shared/components/header/header.component';
+import { AppComponent } from './app.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+  ],
   imports: [
     CommonModule,
     AppRoutingModule
-  ]
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
