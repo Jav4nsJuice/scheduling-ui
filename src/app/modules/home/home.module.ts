5c76c81179f71c2b5b379dc15d2868a705a4fe0c
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 16:54:21 -0400

    feature/S-5: Added the import and added the basis of the component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,13 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home.component';
 import { CarouselComponent } from './components/sliderSection/carousel/carousel.component';
 import { TopStudentsComponent } from './components/topSection/top-students/top-students.component';
-
+import { TopCoursesComponent } from './components/topSection/top-courses/top-courses.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     CarouselComponent,
     TopStudentsComponent,
+    TopCoursesComponent,
   ],
   imports: [
     CommonModule,
