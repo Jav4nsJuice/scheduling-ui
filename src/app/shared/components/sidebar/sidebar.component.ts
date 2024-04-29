e2629aa2aac5add04f8b876aefba22bc31002b00
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 03:13:20 -0400

    feature/S-4: Added styles and responsiveness when hovering in the recently added icons

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -4,6 +4,7 @@ import { Menu, SubMenu } from '../../models/menu.model';
 import { SidebarState } from '../../../core/store/reducers/sidebar.reducer';
 import { State, Store } from '@ngrx/store';
 import { selectCurrentStructure } from 'src/app/core/store/selectors/sidebar.selector';
+import * as SidebarActions from '../../../core/store/actions/sidebar.action';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,9 +13,32 @@ import { selectCurrentStructure } from 'src/app/core/store/selectors/sidebar.sel
 })
 export class SidebarComponent implements OnInit {
   sidebarStructure$: Observable<Menu[]>;
-  ngOnInit(): void {}
-
+  submenuVisible: { [key: string]: boolean } = {};
+  
   constructor(private store: Store<State<SidebarState>>) {
     this.sidebarStructure$ = store.select(selectCurrentStructure);
+  }
+  
+  ngOnInit(): void {}
+
+  updateSidebar(menu: Menu, submenu?: SubMenu) {
+    this.store.dispatch(
+      SidebarActions.updateSidebar({
+        menu: Object.assign({}, menu, { isExpanded: true }),
+        submenu: Object.assign({}, submenu, { isActive: true }),
+      })
+    );
+  }
+
+  showSubmenu(menu: Menu) {
+    this.submenuVisible[menu.label] = true;
+  }
+  
+  hideSubmenu(menu: Menu) {
+    setTimeout(() => {
+      if (!this.submenuVisible[menu.label]) {
+        this.submenuVisible[menu.label] = false;
+      }
+    }, 500);
   }
 }
