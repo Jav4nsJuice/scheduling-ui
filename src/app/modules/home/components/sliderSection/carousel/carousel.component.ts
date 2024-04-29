dcbf0627e450e9aa07a2bbc6579db1b2a1061166
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 10:36:56 -0400

    feature/S-5: Added the interaction with the carousel

--- src/app/modules/home/components/sliderSection/carousel/carousel.component.ts
+++ src/app/modules/home/components/sliderSection/carousel/carousel.component.ts
@@ -8,5 +8,17 @@ import { Component, Input } from '@angular/core';
 export class CarouselComponent {
   @Input() slides;
 
+  currentSlide = 0;
+
   constructor() {}
+
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+  }
+
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides.length ? 0 : next;
+  }
 }
--- src/app/modules/home/pages/home.component.ts
+++ src/app/modules/home/pages/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   slides = [
     { src: "/tx-scheduling/assets/images/slider/slider1.png" },
-    { src: "/tx-scheduling/assets/images/slider/slider2.jpeg" },
+    { src: "/tx-scheduling/assets/images/slider/slider2.png" },
     { src: "/tx-scheduling/assets/images/slider/slider3.jpg" }
   ];
   constructor() { }
