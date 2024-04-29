d413994f057a611d874e45cd298a6d8280ef8bed
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 16:20:13 -0400

    feature/S-5: Implemented the studentCourse counter

--- src/app/modules/home/components/topSection/top-students/top-students.component.ts
+++ src/app/modules/home/components/topSection/top-students/top-students.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { State, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, map, switchMap } from 'rxjs';
 import { StudentsState } from 'src/app/core/store/reducers/student.reducer';
-import { Student } from 'src/app/shared/models/student.model';
-import { getStudents } from '../../../../../core/store/actions/student.action';
-import { selectAllStudents } from '../../../../../core/store/selectors/student.selector';
+import { Student, StudentCourse } from 'src/app/shared/models/student.model';
+import { getStudentCourses, getStudents } from '../../../../../core/store/actions/student.action';
+import { selectAllStudents, selectMyStudentCourses } from '../../../../../core/store/selectors/student.selector';
 
 @Component({
   selector: 'app-top-students',
@@ -14,10 +14,27 @@ import { selectAllStudents } from '../../../../../core/store/selectors/student.s
 export class TopStudentsComponent implements OnInit {
   
   students$: Observable<Student[]> = this.store.select(selectAllStudents);
+  studentCourseCounts$: Observable<{ studentId: string, courseCount: number }[]>;
 
   constructor(private store: Store<State<StudentsState>>) {}
 
   ngOnInit(): void {
+    this.loadStudentsAndCourses();
+  }
+
+  private loadStudentsAndCourses(): void {
     this.store.dispatch(getStudents());
+  
+    this.students$.subscribe(students => {
+      students.forEach(student => {
+        this.store.dispatch(getStudentCourses({ studentId: student.id }));
+      });
+    });
+  }
+
+  getCourseCount(studentId: string): Observable<number> {
+    return this.store.select(selectMyStudentCourses).pipe(
+      map(courses => courses.filter(course => course.studentId === studentId).length)
+    );
   }
 }
