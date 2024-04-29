c83965d202dc690e9be8e33a36b4f8dd66b0ec8a
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 10:45:11 -0400

    feature/S-5: Created the top students component

---
+++ src/app/modules/home/components/topSection/top-students/top-students.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-students',
+  templateUrl: './top-students.component.html',
+  styleUrl: './top-students.component.scss'
+})
+export class TopStudentsComponent {
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home.component';
 import { CarouselComponent } from './components/sliderSection/carousel/carousel.component';
+import { TopStudentsComponent } from './components/topSection/top-students/top-students.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     CarouselComponent,
+    TopStudentsComponent,
   ],
   imports: [
     CommonModule,
