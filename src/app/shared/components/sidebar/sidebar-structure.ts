bf434ca5168f6d3ee2d3e5f95f438ed357c52a5c
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-28 21:40:33 -0400

    feature/S-4: Added the basic strcutre of the sidebar

---
+++ src/app/shared/components/sidebar/sidebar-structure.ts
@@ -0,0 +1,44 @@
+export const sidebarStructure = [
+    {
+      menus: [
+        {
+          id: 'side-bar-home-page',
+          label: 'Home',
+          path: '/home',
+          icon: 'home',
+          isExpanded: true,
+          submenus: [],
+        },
+        {
+          id: 'side-bar-information-page',
+          label: 'Information',
+          path: '/information',
+          icon: 'group',
+          isExpanded: false,
+          submenus: [],
+        },
+        {
+          id: 'side-bar-create',
+          label: 'create-pages',
+          path: null,
+          icon: 'add',
+          isExpanded: false,
+          submenus: [
+            {
+              id: 'create-student',
+              label: 'Create Student',
+              path: '/create/student',
+              isActive: false
+            },
+            {
+              id: 'create-course',
+              label: 'Create Course',
+              path: '/create/course',
+              isActive: false
+            }
+          ],
+        }
+      ],
+    }
+];
+  
