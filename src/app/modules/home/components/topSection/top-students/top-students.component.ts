cc08679eb9294f394f15d27461560c36b3f4f429
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 13:22:39 -0400

    feature/S-5: Recovered information from the API for the student and applied styles

--- src/app/modules/home/components/topSection/top-students/top-students.component.ts
+++ src/app/modules/home/components/topSection/top-students/top-students.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { State, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { StudentsState } from 'src/app/core/store/reducers/student.reducer';
 import { Student } from 'src/app/shared/models/student.model';
+import { getStudents } from '../../../../../core/store/actions/student.action';
+import { selectAllStudents } from '../../../../../core/store/selectors/student.selector';
 
 @Component({
   selector: 'app-top-students',
   templateUrl: './top-students.component.html',
   styleUrl: './top-students.component.scss'
 })
-export class TopStudentsComponent {
+export class TopStudentsComponent implements OnInit {
   
-  students$: Observable<Student[]>;
-  students: Student[] = [];
+  students$: Observable<Student[]> = this.store.select(selectAllStudents);
 
   constructor(private store: Store<State<StudentsState>>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(getStudents());
+  }
 }
