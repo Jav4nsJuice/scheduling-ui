7720784e373d00a608ae6f66a3c0f0e7da3a5546
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 10:02:34 -0400

    feature/S-5: Tested the functionality of the new component Slider

--- src/app/modules/home/components/sliderSection/slider/slider.component.ts
+++ src/app/modules/home/components/sliderSection/slider/slider.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
-  standalone: true,
-  imports: [],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home.component';
+import { SliderComponent } from './components/sliderSection/slider/slider.component';
+
 
 @NgModule({
   declarations: [
     HomeComponent,
+    SliderComponent,
   ],
   imports: [
     CommonModule,
