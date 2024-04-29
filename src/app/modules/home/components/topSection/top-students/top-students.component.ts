729f7d122bd1c3a7a08409cac71a97f7796a7eba
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-29 12:55:19 -0400

    feature/S-5: Added the items container and styles

--- src/app/modules/home/components/topSection/top-students/top-students.component.ts
+++ src/app/modules/home/components/topSection/top-students/top-students.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { State, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { StudentsState } from 'src/app/core/store/reducers/student.reducer';
+import { Student } from 'src/app/shared/models/student.model';
 
 @Component({
   selector: 'app-top-students',
@@ -6,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './top-students.component.scss'
 })
 export class TopStudentsComponent {
+  
+  students$: Observable<Student[]>;
+  students: Student[] = [];
 
+  constructor(private store: Store<State<StudentsState>>) {}
 }
