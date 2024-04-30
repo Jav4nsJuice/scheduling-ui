d362931c8f6c04b9100eddd05bb8a443f941cc8d
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-30 12:48:24 -0400

    feature/S-6: Created the table-content component

--- src/app/modules/table/table.module.ts
+++ src/app/modules/table/table.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { TableRoutingModule } from './table-routing.module';
 import { TableComponent } from './pages/table.component';
 import { MatTableModule } from '@angular/material/table';
-
+import { TableContentComponent } from './components/tableSection/table-content/table-content.component'
 
 @NgModule({
   declarations: [
     TableComponent,
+    TableContentComponent,
   ],
   imports: [
     CommonModule,
