149a57fca553d054405dc7340b7599c28e64a66f
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-30 02:06:48 -0400

    feature/S-5: Added logic to the top-courses component

--- src/app/modules/home/components/topSection/top-courses/top-courses.component.ts
+++ src/app/modules/home/components/topSection/top-courses/top-courses.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { State, Store } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+import { getCourseStudents } from 'src/app/core/store/actions/student.action';
+import { selectAllCourses } from 'src/app/core/store/selectors/course.selector';
+import { selectMyCourseStudents } from 'src/app/core/store/selectors/student.selector';
+import { Course } from 'src/app/shared/models/course.model';
+import { CoursesState } from 'src/app/core/store/reducers/course.reducer';
+import { getCourses } from 'src/app/core/store/actions/course.action';
 
 @Component({
   selector: 'app-top-courses',
   templateUrl: './top-courses.component.html',
   styleUrl: './top-courses.component.scss'
 })
-export class TopCoursesComponent {
+export class TopCoursesComponent implements OnInit {
+  courses$: Observable<Course[]> = this.store.select(selectAllCourses);
+  CourseStudentCounts$: Observable<{ courseId: string, studentCount: number }[]>;
 
+  constructor(private store: Store<State<CoursesState>>) {}
+
+  ngOnInit(): void {
+    this.loadCoursesAndStudents();
+  }
+
+  private loadCoursesAndStudents(): void {
+    this.store.dispatch(getCourses());
+  
+    this.courses$.subscribe(courses => {
+      courses.forEach(course => {
+        this.store.dispatch(getCourseStudents({ courseId: course.id }));
+      });
+    });
+  }
+
+  getStudentCount(courseId: string): Observable<number> {
+    return this.store.select(selectMyCourseStudents).pipe(
+      map(students => students.filter(student => student.courseId === courseId).length)
+    );
+  }
 }
