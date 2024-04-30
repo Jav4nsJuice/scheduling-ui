36048952339699c61e426951595e74e8a72bf419
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-30 13:39:13 -0400

    feature/S-6: Implemented the table content with the right values

--- src/app/modules/table/components/tableSection/table-content/table-content.component.ts
+++ src/app/modules/table/components/tableSection/table-content/table-content.component.ts
@@ -1,4 +1,34 @@
 import { Component } from '@angular/core';
+import { State, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { getCourses } from 'src/app/core/store/actions/course.action';
+import { getStudentCourses, getStudents } from 'src/app/core/store/actions/student.action';
+import { CoursesState } from 'src/app/core/store/reducers/course.reducer';
+import { StudentsState } from 'src/app/core/store/reducers/student.reducer';
+import { selectAllCourses } from 'src/app/core/store/selectors/course.selector';
+import { selectAllStudentCourses, selectAllStudents } from 'src/app/core/store/selectors/student.selector';
+import { Course } from 'src/app/shared/models/course.model';
+import { Student, StudentCourse } from 'src/app/shared/models/student.model';
+
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
 
 @Component({
   selector: 'app-table-content',
@@ -6,5 +36,45 @@ import { Component } from '@angular/core';
   styleUrl: './table-content.component.scss'
 })
 export class TableContentComponent {
+  constructor(
+    private store: Store<State<StudentsState>>,
+    private storeCourse: Store<State<CoursesState>>
+  ) {}
 
+  studentCourses$: Observable<StudentCourse[]> = this.store.select(selectAllStudentCourses);
+  students$: Observable<Student[]> = this.store.select(selectAllStudents);
+
+  courses$: Observable<Course[]> = this.storeCourse.select(selectAllCourses);
+
+  dataSource: { 
+    firstName: string,
+    lastName: string, 
+    title: string, 
+    description: string 
+  }[] = [];
+
+  ngOnInit(): void {
+    this.store.dispatch(getStudents());
+    this.store.dispatch(getStudentCourses());
+    this.store.dispatch(getCourses());
+
+    this.studentCourses$.subscribe(studentCourses => {
+      this.students$.subscribe(students => {
+        this.courses$.subscribe(courses => {
+          this.dataSource = studentCourses.map(studentCourse => {
+            const student = students.find(student => student.id === studentCourse.studentId);
+            const course = courses.find(course => course.id === studentCourse.courseId);
+            return { 
+              firstName: student ? student.firstName : '',
+              lastName: student ? student.lastName : '',
+              title: course ? course.title : '',
+              description: course ? course.description : ''
+            };
+          });
+        });
+      });
+    });
+  }
+
+  displayedColumns: string[] = ['firstName', 'lastName', 'courseTitle', 'courseDescription'];
 }
