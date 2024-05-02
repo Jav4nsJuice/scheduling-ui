dead757e8c45e4ae5e74071e56e83fd66bc9aa47
Javier Ferrel <javans.ferrel@gmail.com>
2024-05-02 03:04:59 -0400

    feature/S-7: Implemented the course select and import it

--- src/app/modules/table/components/tableSection/add-resource-dialog/add-resource-dialog.component.ts
+++ src/app/modules/table/components/tableSection/add-resource-dialog/add-resource-dialog.component.ts
@@ -6,6 +6,10 @@ import { Student } from 'src/app/shared/models/student.model';
 import { StudentsState } from 'src/app/core/store/reducers/student.reducer';
 import { selectAllStudents } from 'src/app/core/store/selectors/student.selector';
 import { getStudents } from 'src/app/core/store/actions/student.action';
+import { CoursesState } from 'src/app/core/store/reducers/course.reducer';
+import { Course } from 'src/app/shared/models/course.model';
+import { selectAllCourses } from 'src/app/core/store/selectors/course.selector';
+import { getCourses } from 'src/app/core/store/actions/course.action';
 
 export interface DialogData {
   animal: string;
@@ -22,9 +26,11 @@ export class AddResourceDialogComponent implements OnInit {
     public dialogRef: MatDialogRef<AddResourceDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private store: Store<State<StudentsState>>,
+    private storeCourse: Store<State<CoursesState>>,
   ) {}
 
   students$: Observable<Student[]> = this.store.select(selectAllStudents);
+  courses$: Observable<Course[]> = this.storeCourse.select(selectAllCourses);
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -32,5 +38,6 @@ export class AddResourceDialogComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(getStudents());
+    this.storeCourse.dispatch(getCourses());
   }
 }
