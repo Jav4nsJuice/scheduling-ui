c220264fca5c2ed9262b6bf199e91659267af690
Javier Ferrel <javans.ferrel@gmail.com>
2024-05-03 04:20:30 -0400

    feature/S-12: Implemented the imports in the courses module and implemented base of the courses container

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -2,8 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './pages/courses.component'
-
+import { CoursesComponent } from './pages/courses.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +26,18 @@ import { CoursesComponent } from './pages/courses.component'
   ],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatSelectModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModule,
   ]
 })
 export class CoursesModule { }
