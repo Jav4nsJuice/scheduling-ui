725e432fd854fde6b6c96a85564b15a11682dbcd
Javier Ferrel <javans.ferrel@gmail.com>
2024-05-03 02:26:52 -0400

    feature/S-9: Implemented the logic behind the craete student

--- src/app/modules/students/components/create-student-dialog/create-student-dialog.component.ts
+++ src/app/modules/students/components/create-student-dialog/create-student-dialog.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { State, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { addStudent } from 'src/app/core/store/actions/student.action';
+import { StudentsState } from 'src/app/core/store/reducers/student.reducer';
+import { selectAllStudents } from 'src/app/core/store/selectors/student.selector';
+import { Student } from 'src/app/shared/models/student.model';
 
 @Component({
   selector: 'app-create-student-dialog',
   templateUrl: './create-student-dialog.component.html',
   styleUrl: './create-student-dialog.component.scss'
 })
-export class CreateStudentDialogComponent {
+export class CreateStudentDialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<CreateStudentDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Student,
+    private store: Store<State<StudentsState>>,
+  ) {}
 
+  @Input() control:FormControl;
+
+  ngOnInit(): void {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  createStudent(): void {
+    console.log(this.data.id);
+    console.log(this.data.firstName);
+    console.log(this.data.lastName);
+    this.store.dispatch(addStudent({ student: this.data }));
+  }
 }
--- src/app/modules/students/students.module.ts
+++ src/app/modules/students/students.module.ts
@@ -8,6 +8,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { CreateStudentDialogComponent } from './components/create-student-dialog/create-student-dialog.component'
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,6 +34,14 @@ import { CreateStudentDialogComponent } from './components/create-student-dialog
     MatFormFieldModule,
     MatTableModule,
     MatInputModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatSelectModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModule,
   ]
 })
 export class StudentsModule { }
